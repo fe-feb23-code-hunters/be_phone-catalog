1097b71e33ef4d1574ba45de3fd36b1ec14baed0
Daria <hamdsad8@gmail.com>
2023-06-08 22:02:15 +0300

    prepare for deploy

--- build/index.js
+++ build/index.js
@@ -1,22 +1,15 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const dbInit = require('./utils/dbInit');
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
-
 dbInit();
-
 app.use(cors());
-
 app.get('/', (_req, res) => {
-  res.send('Hello World, from express');
+    res.send('Hello World, from express');
 });
-
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running in port ${PORT}`);
+    // eslint-disable-next-line no-console
+    console.log(`Server is running in port ${PORT}`);
 });
---
+++ build/utils/dbInit.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_typescript_1 = require("sequelize-typescript");
+require('dotenv').config();
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const URI = `postgres://${dbUser}:${dbPassword}@${dbHost}/${dbName}`;
+const dbInit = () => {
+    try {
+        const db = new sequelize_typescript_1.Sequelize(URI, {
+            dialectOptions: {
+                ssl: true,
+            },
+        });
+        // eslint-disable-next-line no-console
+        console.log('DB successfully initialized');
+        return db;
+    }
+    catch (error) {
+        // eslint-disable-next-line no-console
+        console.log('DB failed to connect', error);
+    }
+};
+module.exports = dbInit;
