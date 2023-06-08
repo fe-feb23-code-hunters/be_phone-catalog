4e7bd351d40c59949478830130ea8e2bd65979f8
Anna Latusha <annapop03072701@gmail.com>
2023-06-08 17:59:46 +0300

    setup Postgress on API through Neon, connect to DB from Node.js (use Sequelize)

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const dbInit = require('./utils/dbInit');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT|| 3000;
 
 const app = express();
+
+dbInit();
 
 app.use(cors());
 
---
+++ src/utils/dbInit.ts
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize-typescript";
+require('dotenv').config();
+
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+const URI = `postgres://${dbUser}:${dbPassword}@${dbHost}/${dbName}`;
+
+export const dbInit = () => {
+  try {
+    const db = new Sequelize(
+      URI,
+      {
+        dialectOptions: {
+          ssl: true,
+        }
+      }
+    );
+
+    console.log('DB successfully initialized');
+
+    return db;
+  } catch (error) {
+    console.log('DB failed to connect', error);
+  }
+}
