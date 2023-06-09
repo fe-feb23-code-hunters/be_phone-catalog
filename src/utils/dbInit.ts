a083c81f28989fe8df5f5f0322937d7f74c0a892
Anna Latusha <annapop03072701@gmail.com>
2023-06-09 19:49:37 +0300

    Create product and phone  tables

--- src/utils/dbInit.ts
+++ src/utils/dbInit.ts
@@ -1,30 +1,37 @@
+/* eslint-disable no-console */
 import { Sequelize } from 'sequelize-typescript';
-
+import { models } from '../models';
 require('dotenv').config();
 
-const dbHost = process.env.DB_HOST;
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+const URI = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}`
++ `@${process.env.DB_HOST}/${process.env.DB_NAME}`;
 
-const URI = `postgres://${dbUser}:${dbPassword}@${dbHost}/${dbName}`;
-
-const dbInit = () => {
+export const dbInit = () => {
   try {
-    const db = new Sequelize(URI, {
-      dialectOptions: {
-        ssl: true,
+    const db = new Sequelize(
+      URI,
+      {
+        models,
+        dialectOptions: {
+          ssl: true,
+        },
       },
-    });
+    );
 
-    // eslint-disable-next-line no-console
+    console.log(process.env.DB_NAME);
+    console.log(process.env.DB_USER);
+    console.log(process.env.DB_PASSWORD);
+    console.log(process.env.DB_HOST);
+
     console.log('DB successfully initialized');
 
     return db;
   } catch (error) {
-    // eslint-disable-next-line no-console
     console.log('DB failed to connect', error);
   }
 };
+<<<<<<< HEAD
 
 export default dbInit;
+=======
+>>>>>>> f09dbc3 (Create product and phone  tables)
